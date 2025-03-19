bb0eb146a9fcc9e6fbef62363dbd501c6c5417e7
Jessica Estes <jessica.m.estes@gmail.com>
2025-03-19 19:33:54 -0400

    Initial Commit

---
+++ TestingTesting123/TestingTesting123App.swift
@@ -0,0 +1,17 @@
+//
+//  TestingTesting123App.swift
+//  TestingTesting123
+//
+//  Created by Jessica Estes on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TestingTesting123App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
